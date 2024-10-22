d031bfd22a6d04c3c277b34537f4238740b6543f
aashanamir <emailstormfacts@gmail.com>
2024-10-22 15:39:25 +0500

    Some Changes IN Database and cookie

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,12 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    await mongoose.connect("mongodb+srv://emaildasantservices:12345@braxas.8roq6.mongodb.net/?retryWrites=true&w=majority&appName=Braxas" , {
-      dbName : "Braxas"
+    await mongoose.connect("mongodb+srv://emaildasantservices:12345@braxas.8roq6.mongodb.net/?retryWrites=true&w=majority&appName=Braxas", {
+      dbName: "Braxas",
     });
     console.log("Database connected successfully");
   } catch (error) {
     console.log(error);
     console.log("Database Error");
-  } 
-
+  }
 }
